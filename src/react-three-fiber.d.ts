4815d8c31713d4d85405c625e1ecc12415d3f0cd
Crawron <azertyaaron@gmail.com>
2019-11-08 12:08:45 -0500

    Updated react-three-fiber

--- src/react-three-fiber.d.ts
+++
@@ -1,9 +0,0 @@
-// here until types are fixed
-// https://github.com/react-spring/react-three-fiber/issues/239
-declare module "react-three-fiber"
-
-declare namespace JSX {
-  interface IntrinsicElements {
-    [element: string]: any
-  }
-}
