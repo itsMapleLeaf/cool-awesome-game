355c8b42570a8329989c502888d89624eae2860f
kingdaro <kingdaro@gmail.com>
2019-11-13 00:50:16 -0600

    server: add disconnect message

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,13 +3,18 @@ import ws from "ws"
 const server = new ws.Server({ port: 3001 })
 
 server.on("connection", (socket, request) => {
-  console.log(`connection from ${request.connection.remoteAddress}`)
+  const address = request.connection.remoteAddress
+  console.log(`connected: ${address}`)
 
   socket.send(JSON.stringify({ message: "hi" }))
 
   socket.on("message", (data) => {
     const json = JSON.parse(String(data))
     console.log(json)
+  })
+
+  socket.on("close", () => {
+    console.log(`disconnected: ${address}`)
   })
 })
 
