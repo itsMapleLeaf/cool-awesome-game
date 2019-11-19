5eab48cab6a5c3f1d75ca4ff26aa609c23009866
kingdaro <kingdaro@gmail.com>
2019-11-18 20:55:53 -0600

    simple server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,15 @@
-import { GameServer } from "./GameServer"
+import { Server } from "colyseus"
+import express from "express"
+import { createServer } from "http"
+const app = express()
 
-new GameServer()
+app.use(express.json())
+
+const gameServer = new Server({
+  server: createServer(app),
+})
+
+const port = Number(process.env.port) || 3001
+gameServer.listen(port, undefined, undefined, () => {
+  console.log(`listening on http://localhost:${port}`)
+})
