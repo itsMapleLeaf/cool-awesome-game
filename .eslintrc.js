ae5af8e5581910a550e845ed926b34e3aeb13983
kingdaro <kingdaro@gmail.com>
2019-11-20 13:02:36 -0600

    set up prettier + format script

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "eslint-config-prettier",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "no-unused-vars": "off",
+    "react/jsx-key": "warn",
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+  },
+}
