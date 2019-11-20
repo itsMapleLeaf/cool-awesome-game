57299a9ac26b39af7e2fc1707f4c0df79accef45
kingdaro <kingdaro@gmail.com>
2019-11-20 13:19:31 -0600

    fragment is dumb

--- src/client/Game.tsx
+++ src/client/Game.tsx
@@ -4,14 +4,10 @@ import { GameState } from "../core/gameState"
 type Props = { state: GameState }
 
 export default function Game({ state }: Props) {
-  return (
-    <>
-      {state.players.map((player) => (
-        <mesh key={player.id} position={[player.position, 0, 0]}>
-          <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-          <meshNormalMaterial attach="material" />
-        </mesh>
-      ))}
-    </>
-  )
+  return state.players.map((player) => (
+    <mesh key={player.id} position={[player.position, 0, 0]}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshNormalMaterial attach="material" />
+    </mesh>
+  )) as any
 }
